640092f879786d3d915ffd0c1237c7d1facd3380
bejar <bejar@lsi.upc.edu>
2014-12-02 11:57:23 +0100

    Refactoring gettweets

--- src/SuperHub/Constants.py
+++ src/SuperHub/Constants.py
@@ -33,9 +33,9 @@ msqldbs = 'SuperHub'
 bcncoord = (41.24, 41.52, 1.95, 2.34)
 milancoord = (45.33, 45.59, 9.03, 9.37)
 hlsnkcoord = (60.02, 60.30, 24.72, 25.10)
-pariscoord = (48.37, 48.65, 2.01, 2.41)
+pariscoord = (48.45, 48.54, 2.16, 2.25)
 londoncoord = (51.16, 51.45, -0.13, 0.27)
-berlincoord = (52.16, 52.45, 13.03, 13.43)
+berlincoord = (52.24, 52.36, 13.15, 13.32)
 
 homepath = '/home/bejar/Data/SuperHub/'
 bcnparam = (mgdbbcn, bcncoord, 'bcn')
