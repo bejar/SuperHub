7a7e2ce65abfc5814c63d171bf41d41725643436
bejar <bejar@lsi.upc.edu>
2015-02-02 08:53:55 +0100

    Adding Instagram Getters

--- src/Instagram/IGCallback.py
+++ src/Instagram/IGCallback.py
@@ -20,3 +20,40 @@ IGCallback
 __author__ = 'bejar'
 
 
+from flask import Flask, request, render_template
+import socket
+import time
+
+hostname = socket.gethostname()
+port = 9999
+
+app = Flask(__name__)
+city_status = {}
+
+@app.route("/Instagram")
+def callback():
+    """
+    process the callback from Instagram
+
+    @return:
+    """
+    global city_status
+
+    city = request.args['content']
+    res = city.json()
+
+    return res
+
+@app.route('/Status')
+def info():
+    """
+    Status de las ciudades
+    """
+    global city_status
+
+    return 'Ok'
+
+if __name__ == '__main__':
+
+    # Ponemos en marcha el servidor Flask
+    app.run(host='0.0.0.0', port=9999, debug=False)
