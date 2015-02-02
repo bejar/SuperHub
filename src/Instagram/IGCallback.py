997624415075ded775f4a262142e5b4d2af7346f
bejar <bejar@lsi.upc.edu>
2015-02-02 08:35:54 +0100

    Adding Instagram Getters

---
+++ src/Instagram/IGCallback.py
@@ -0,0 +1,22 @@
+"""
+.. module:: IGCallback
+
+IGCallback
+*************
+
+:Description: IGCallback
+
+    
+
+:Authors: bejar
+    
+
+:Version: 
+
+:Created on: 02/02/2015 8:34 
+
+"""
+
+__author__ = 'bejar'
+
+
