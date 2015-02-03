670f3d82817def0910ca36a8f1f341f2cfe75315
bejar <bejar@lsi.upc.edu>
2015-02-03 11:18:12 +0100

    Changing Twitter to directly insert in MongoDB

--- SuperHub/Twitter/GetTweets-London.py
+++ SuperHub/Twitter/GetTweets-London.py
@@ -19,14 +19,24 @@ gettweets
 
 __author__ = 'bejar'
 
-from TwitterGetterTimeout import get_tweets, config_logger
+from TwitterGetterTimeoutMongo import get_tweets, config_logger
 from time import sleep
+from Parameters.Pconstants import mglocal
+from pymongo import MongoClient
+
 
 
 # City parameter
 city = 'london'
 
-logger = config_logger(silent=True)
+logger = config_logger(silent=False)
+mgdb = mglocal[0]
+client = MongoClient(mgdb)
+db = client.local
+db.authenticate(mglocal[2], password=mglocal[3])
+col = db['Twitter']
+
+
 while True:
-    get_tweets(city, logger, inform=100)
+    get_tweets(city, logger, col, inform=100)
     sleep(5)
